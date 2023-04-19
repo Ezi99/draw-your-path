706326833acc3ffc3f59d7147502fcbb85cb94ad
YousefDake <118476529+YousefDake@users.noreply.github.com>
2023-04-19 20:32:05 +0300

    Health Bar and Enemy Attack

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth=100;
+    public int currentHealth;
+    public HealthBar healthbar;
+    void Start()
+    {
+        currentHealth=maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+    public void TakeDamage(int dmg)
+    {
+        currentHealth-=dmg;
+        healthbar.SetHealth(currentHealth);
+    }
+}
