d91faf7317ce6e087dfb2a9de4d4255deedc3ed4
ismael jubran <ismaeljubranja@mta.ac.il>
2023-04-20 21:57:01 +0300

    sorted some stuff

--- Assets/scripts/EnemySwordAttack.cs
+++ Assets/scripts/EnemySwordAttack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class swordAttack : MonoBehaviour
+public class EnemySwordAttack : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
