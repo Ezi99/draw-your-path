ac289b2ad4ce8324898924cc6c7ef18b5c8c5b40
ismael jubran <ismaeljubranja@mta.ac.il>
2023-05-29 18:50:22 +0300

    player can block and kill using fireball+test on death animation

--- Assets/scripts/Arrow.cs
+++ Assets/scripts/Arrow.cs
@@ -6,15 +6,15 @@ public class Arrow : MonoBehaviour
 {
     private void Start()
     {
-        Destroy(gameObject,10);
+        Destroy(gameObject, 10);
     }
-   void OnCollisionEnter(Collision collision)
-{
-    if(collision.gameObject.CompareTag("Player"))
+    void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Arrow Hit");
-        collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(20);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Arrow Hit");
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(20);
+            Destroy(gameObject); // Destroy the arrow game object
+        }
     }
-    Destroy(gameObject); // Destroy the arrow game object
-}
 }
--- Assets/scripts/Controllers/ErikaScript.cs
+++ Assets/scripts/Controllers/ErikaScript.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ErikaScript : MonoBehaviour
 {
-    private Animator animator;
+    
+    public List<Rigidbody> rigidbodies;
+    private Animator m_Animator;
     public GameObject arrowObj;
     public Transform arrowPoint;
     public Transform playerTransform;
@@ -12,10 +15,12 @@ public class ErikaScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        m_Animator = GetComponent<Animator>();
+       
     }
     public void shoot()
     {
+        Debug.Log("Arrow shot");
         GameObject arrow = Instantiate(arrowObj, arrowPoint.position, Quaternion.identity);
         Vector3 direction = playerTransform.position+new Vector3(0,1.7f,0) - arrowPoint.position;
         Quaternion rotation = Quaternion.LookRotation(direction);
@@ -29,13 +34,19 @@ public class ErikaScript : MonoBehaviour
         Debug.Log($"Erika Health -{dmg} current health {health}");
         if (health<=0)
         {
-            Die();
+                m_Animator.enabled = false;
+            //Die();
+            foreach(var item in rigidbodies)
+            {
+                item.isKinematic = false;
+                item.useGravity = true;
+            }
         }
     }
     private void Die()
     {
         Debug.Log("Death animation triggered");
-        animator.SetBool("Death", true);
+        m_Animator.SetBool("Death", true);
     }
     public void destroy()
     {
