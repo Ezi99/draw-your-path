3a53438f30eb40b81398b186862699dd31e7c7bc
ismael jubran <ismaeljubranja@mta.ac.il>
2023-05-30 23:52:31 +0300

    prepare to merge

--- Assets/scripts/Controllers/ErikaScript.cs
+++ Assets/scripts/Controllers/ErikaScript.cs
@@ -5,38 +5,38 @@ using UnityEngine.AI;
 
 public class ErikaScript : MonoBehaviour
 {
-    
+
     public List<Rigidbody> rigidbodies;
     private Animator m_Animator;
     public GameObject arrowObj;
     public Transform arrowPoint;
     public Transform playerTransform;
-    public int health=100;
+    public int health = 100;
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
-       
+
     }
     public void shoot()
     {
         Debug.Log("Arrow shot");
         GameObject arrow = Instantiate(arrowObj, arrowPoint.position, Quaternion.identity);
-        Vector3 direction = playerTransform.position+new Vector3(0,1.7f,0) - arrowPoint.position;
+        Vector3 direction = playerTransform.position + new Vector3(0, 1.7f, 0) - arrowPoint.position;
         Quaternion rotation = Quaternion.LookRotation(direction);
         arrow.transform.rotation = rotation;
         arrow.GetComponent<Rigidbody>().AddForce(arrow.transform.forward * 25, ForceMode.Impulse);
     }
     public void takeDamage(int dmg)
     {
-        
+
         health -= dmg;
         Debug.Log($"Erika Health -{dmg} current health {health}");
-        if (health<=0)
+        if (health <= 0)
         {
-                m_Animator.enabled = false;
+            m_Animator.enabled = false;
             //Die();
-            foreach(var item in rigidbodies)
+            foreach (var item in rigidbodies)
             {
                 item.isKinematic = false;
                 item.useGravity = true;
