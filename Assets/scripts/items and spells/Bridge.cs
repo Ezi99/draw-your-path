44e6ba80b3d6c17b240495b0a1ed5e7ee97189a3
ismael jubran <ismaeljubranja@mta.ac.il>
2023-07-25 15:04:05 +0300

    countdown on bridge + increased velocity of fireball

--- Assets/scripts/items and spells/Bridge.cs
+++ Assets/scripts/items and spells/Bridge.cs
@@ -1,14 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bridge : MonoBehaviour
 {
-    private int lifeSpan;
+    public Text m_Timer1;
+    public Text m_Timer2;
+    public int m_TimeLeft = -1;
+    public bool m_CountDown;
+
+    private void Update()
+    {
+        if (m_TimeLeft >= 0 && m_CountDown == true)
+        {
+            UpdateTimer(m_TimeLeft);
+            m_CountDown = false;
+        }
+
+    }
 
     public void DespawnCountDown(int lifeSpan)
     {
         Destroy(gameObject, lifeSpan);
+        m_TimeLeft = lifeSpan;
+        m_CountDown = true;
     }
 
     public void WhenSelected()
@@ -19,5 +36,18 @@ public class Bridge : MonoBehaviour
     public void WhenExited()
     {
         gameObject.layer = LayerMask.NameToLayer("bridge");
+    }
+
+    private void decreaseTime()
+    {
+        m_TimeLeft--;
+        m_CountDown = true;
+    }
+
+    private void UpdateTimer(int currentTime)
+    {
+        m_Timer1.text = currentTime.ToString();
+        m_Timer2.text = currentTime.ToString();
+        Invoke("decreaseTime", 1);
     }
 }
